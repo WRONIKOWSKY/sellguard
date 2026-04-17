0a201e3ede2c666f9e856d460f63b8209715769b
WRONIKOWSKY <neaume.contact@gmail.com>
2026-04-17 11:35:35 +0200

    Update _app.js

--- sellguard/pages/_app.js
+++ sellguard/pages/_app.js
@@ -1,5 +1,10 @@
-import "../styles/globals.css";
 import { LangProvider } from "../contexts/LangContext";
+import "../styles/globals.css";
+
 export default function App({ Component, pageProps }) {
-  return <LangProvider><Component {...pageProps} /></LangProvider>;
+  return (
+    <LangProvider>
+      <Component {...pageProps} />
+    </LangProvider>
+  );
 }
